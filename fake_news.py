f68ae3f261ab93ab19c4f5138b0b7418194acc87
Prithviraj G17🕸️ <148732155+Prithvirajg17@users.noreply.github.com>
2024-06-04 00:48:20 +0530

    fake_news.py

--- fake_news.py
+++ fake_news.py
@@ -6,12 +6,7 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import make_pipeline
 from sklearn.metrics import accuracy_score, classification_report
 
-# Add custom CSS to hide the GitHub icon
-hide_github_icon = """
-#GithubIcon {
-  visibility: hidden;
-}
-"""
+
 st.markdown(hide_github_icon, unsafe_allow_html=True)
 
 # Your app code goes here
